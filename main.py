8d317b6ddd87fb5ca193bfa4258a0076ec353005
orucfatih <orucfatih4545@gmail.com>
2024-06-12 12:54:30 +0300

    DataFrame oluşturuldu.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from personel import Personel
 from hemsire import Hemsire
 from doktor import Doktor
 from hasta import Hasta
+import pandas as pd
 
 personeller = []
 doktorlar = []
@@ -136,14 +137,39 @@ def goruntule():
         print("Yanlis bir giris yaptiniz! Tekrar deneyin.")
         goruntule()
 
+def dataframe():
+    tum_nesneler = personeller + doktorlar + hemsireler + hastalar 
+    data = {'sinif': [type(obj).__name__ for obj in tum_nesneler],
+    'personel_no': [obj.get_personel_no() if hasattr(obj, 'get_personel_no') else None for obj in tum_nesneler],
+    'ad': [obj.get_ad() for obj in tum_nesneler],
+    'soyad': [obj.get_soyad() for obj in tum_nesneler],
+    'departman': [obj.get_departman() if hasattr(obj, 'get_departman') else None for obj in tum_nesneler],
+    'maas': [obj.get_maas() if hasattr(obj, 'get_maas') else None for obj in tum_nesneler],
+    'uzmanlik': [obj.get_uzmanlik() if hasattr(obj, 'get_uzmanlik') else None for obj in tum_nesneler],
+    'deneyim_yili': [obj.get_deneyim_yili() if hasattr(obj, 'get_deneyim_yili') else None for obj in tum_nesneler],
+    'hastane': [obj.get_hastane() if hasattr(obj, 'get_hastane') else None for obj in tum_nesneler],
+    'calisma_saati': [obj.get_calisma_saati() if hasattr(obj, 'get_calisma_saati') else None for obj in tum_nesneler],
+    'sertifika': [obj.get_sertifika() if hasattr(obj, 'get_sertifika') else None for obj in tum_nesneler],
+    'hasta_no': [obj.get_hasta_no() if hasattr(obj, 'get_hasta_no') else None for obj in tum_nesneler],
+    'dogum_tarihi': [obj.get_dogum_tarihi() if hasattr(obj, 'get_dogum_tarihi') else None for obj in tum_nesneler],
+    'hastalik': [obj.get_hastalik() if hasattr(obj, 'get_hastalik') else None for obj in tum_nesneler],
+    'tedavi': [obj.get_tedavi() if hasattr(obj, 'get_tedavi') else None for obj in tum_nesneler]}
+
+    df = pd.DataFrame(data)
+    df = df.fillna(0)
+    print(df.to_string())
+    
+
 def main():
     while True:
         try:
-            secenek = input("Ekleme icin 1\nGoruntuleme icin 2 tuslayiniz.\n(Cikmak icin 0 tuslayiniz.)")
+            secenek = input("Ekleme icin 1\nGoruntuleme icin 2 \nDataFrame islemleri icin 3 tuslayiniz.\n(Cikmak icin 0 tuslayiniz.)")
             if secenek == "1":
                 ekle()
             elif secenek =="2":
                 goruntule()
+            elif secenek == "3":
+                dataframe()
             elif secenek == "0":
                 print("Program sonlandiriliyor...")
                 break
